8609439c928d49c403d8058651fd60525cad7288
Robert Novotny <robert.novotny@dcoresystems.com>
2016-04-07 11:17:28 +0200

    Initial commit

---
+++ src/main/java/sk/eastcode/weather/WeatherController.java
@@ -0,0 +1,20 @@
+package sk.eastcode.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class WeatherController {
+    @RequestMapping
+    public String getWeather() {
+        return "Warm";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherController.class, args);
+    }
+}
